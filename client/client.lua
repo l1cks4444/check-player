ce57f38c6e9ce78055839f48e2f14981ce2ff2e8
L1cKs #444 <138713607+l1cks4444@users.noreply.github.com>
2025-09-21 20:26:01 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,24 +1,24 @@
 RegisterCommand("check", function(source, args, RawCommand)
     local group = GetPlayerGroup(source)
     if not group == "admin" or group == "superadmin" or group == "mod" then
-        print("keine rechte brudi")
+        print("you need to be admin group")
         return
     end
     if args[1]  == nil then
-        print("Usage: /check [playerID]")
+        print("/check [playerID]")
         return
     end
     if args[1] == source then
-        print("You cannot check yourself.")
+        print("cant check yourself")
         return
     end
     local targetId = tonumber(args[1])
     if args[1] then
         TriggerServerEvent("delta_check:checkPlayer", targetId)
-        print("Checking player ID: " .. targetId)
-        print("Done. Check Discord Log!")
+        print("checking player ID: " .. targetId)
+        print("Done. check discord log!")
     else
-        print("Invalid player ID.")
+        print("Invalid player id.")
     end
     
 end, true)
